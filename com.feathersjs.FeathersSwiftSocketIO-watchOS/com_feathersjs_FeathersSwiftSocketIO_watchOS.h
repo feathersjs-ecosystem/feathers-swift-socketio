6d12dcab11f12c9afe3148ea928f6c2ad43f5d22
Brendan Conron <conronb@gmail.com>
2018-01-29 10:39:15 -0800

    added watchos framework

---
+++ com.feathersjs.FeathersSwiftSocketIO-watchOS/com_feathersjs_FeathersSwiftSocketIO_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  com_feathersjs_FeathersSwiftSocketIO_watchOS.h
+//  com.feathersjs.FeathersSwiftSocketIO-watchOS
+//
+//  Created by Brendan Conron on 1/29/18.
+//  Copyright © 2018 FeathersJS. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for com_feathersjs_FeathersSwiftSocketIO_watchOS.
+FOUNDATION_EXPORT double com_feathersjs_FeathersSwiftSocketIO_watchOSVersionNumber;
+
+//! Project version string for com_feathersjs_FeathersSwiftSocketIO_watchOS.
+FOUNDATION_EXPORT const unsigned char com_feathersjs_FeathersSwiftSocketIO_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <com_feathersjs_FeathersSwiftSocketIO_watchOS/PublicHeader.h>
+
+
